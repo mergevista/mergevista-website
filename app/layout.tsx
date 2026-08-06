e912c24a2ad894b77392385d46b145d2c434e065
strokesandseams <rajeev@strokesandseams.com>
2026-08-05 21:22:49 -0500

    feat: add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const siteUrl = "https://www.mergevista.com";
@@ -92,6 +93,16 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XJ3E9KR8JG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XJ3E9KR8JG');`}
+        </Script>
       </body>
     </html>
   );
